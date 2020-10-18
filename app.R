713928d1978e34a5e18005736cfbe62e649cb6dd
Patrick Wheeler <pwheeler636@gmail.com>
2020-10-17 22:40:46 -0400

    added dynamic features to map

--- app.R
+++ app.R
@@ -47,22 +47,26 @@ ui <- navbarPage("Philly Bail Stats",
               #Bail Amount Vs Frequency
               selectInput("histograms",
                           "Which Histogram Do Ya Want?",
-                          choices=c("Offense Data Vs Denstiny", "Bail Amount Vs Frequency", "Date of Birth Vs Bail Amount in Thousands"),
+                          choices=c("Offense Data Vs Denstiny", "Bail Amount Vs Frequency", "Date of Birth Vs Bail Amount"),
                           selected=1
                           )
                 ),
             mainPanel("Histograms",
                 conditionalPanel(
                   condition = "input.histograms == 'Offense Data Vs Denstiny'",
-                  plotOutput("hist1")
+                  plotOutput("hist1", click = "plot_click"),
+                    verbatimTextOutput("info")
                 ),
                 conditionalPanel(
                   condition = "input.histograms == 'Bail Amount Vs Frequency'",
-                  plotOutput("hist2")
+                  plotOutput("hist2", click = "plot_click"),
+                  verbatimTextOutput("info2")
                  ),
                  conditionalPanel(
-                   condition = "input.histograms == 'Date of Birth Vs Bail Amount in Thousands'",
-                   plotOutput("hist3")
+                   condition = "input.histograms == 'Date of Birth Vs Bail Amount'",
+                   plotOutput("hist3", click = "plot_click"),
+                   #tableOutput("test")
+                   verbatimTextOutput("info3")
                  )
             ),
           ),
@@ -92,30 +96,54 @@ server <- function(input, output) {
   # output$normal <- renderPlot({
   #   plot(x, y)
   # })
+  my_data <- as_tibble(data)
+  my_data$offense_date <- as.Date(data$offense_date, format = "%m/%d/%y")
+  my_data2015 <- my_data %>% filter(offense_date > '2015-01-01')
+  my_dataBail <- my_data %>% filter(bail_amount < 100000)
+  my_dataDOB <- foo(as.Date(data$dob, format = "%m/%d/%y"), 1920)
+  bail_in_thousands <- data$bail_amount / 1000
   output$hist1 <- renderCachedPlot({
-    my_data <- as_tibble(data)
-    my_data$offense_date <- as.Date(data$offense_date, format = "%m/%d/%y")
-    x <- my_data %>% filter(offense_date > '2015-01-01')
-    hist(x$offense_date, breaks = 'months',
+    #my_data <- as_tibble(data)
+    hist(my_data2015$offense_date, breaks = 'months',
          xlab = "Offense Date",
          ylab = "Density")
   }, cacheKeyExpr = { input$n })
   output$hist2 <- renderCachedPlot({
-    my_data <- as_tibble(data)
-    y <- my_data %>% filter(bail_amount < 100000)
-    hist(y$bail_amount, breaks = "fd", 
+    #my_data <- as_tibble(data)
+    hist(my_dataBail$bail_amount, breaks = "fd", 
          main = "Histogram of Bail Amount",
          xlab = "Bail Amount",
          ylab = "Frequency")
   }, cacheKeyExpr = { input$n })
   output$hist3 <- renderCachedPlot({
-    x <- foo(as.Date(data$dob, format = "%m/%d/%y"), 1920)
-    
-    bail_in_thousands <- data$bail_amount / 1000
-    plot(x, bail_in_thousands,
+    plot(my_dataDOB, bail_in_thousands,
          xlab = "Date of Birth",
-         ylab = "Bail Amount in Thousands")
+         ylab = "Bail Amount in Thousands of $")
   }, cacheKeyExpr = { input$n })
+  output$info <- renderText({
+    paste0("density = ", input$plot_click$y)
+  })
+  output$info2 <- renderText({
+    y <- input$plot_click$y
+    if (is.numeric(y)) {#only render if y is numeric.
+      paste0("bail = $", round(input$plot_click$x, digits=2), "\nfrequency = ", round(y))
+    }
+    else {
+      paste0("bail = $\nfrequency = ")
+    }
+  })
+  output$info3 <- renderText({
+    y <- input$plot_click$y
+    if (is.numeric(y)) {
+      paste0("bail = $", round(y, digits=2))
+    }
+    else {
+      paste0("bail = $")
+    }
+  })
+  # output$test <- renderText({
+  #   nearPoints(my_data, input$plot_click, xvar = "dob", yvar = "bail_amount")
+  # })
   output$table <- renderDataTable({
     table
   })
